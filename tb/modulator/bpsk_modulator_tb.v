19fc70fef0a55bba544bae4b240dc4e6a87d48ab
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2023-10-02 15:11:37 +0300

    Fix modulator and tb (now bpsk working)

--- tb/modulator/bpsk_modulator_tb.v
+++ tb/modulator/bpsk_modulator_tb.v
@@ -1,13 +1,16 @@
-`timescale 1ns / 1ns
+`timescale 1ps / 1ps
 
 module bpsk_modulator_tb();
 
 reg         clk;
 reg         arst;
 reg         en;
-reg         s;
+reg [8:0]   in;
 
 wire [11:0] signal_out;
+wire [7:0] sine_cnt;
+wire [2:0] sel_cnt;
+wire       sel;
 
 integer i;
 
@@ -18,27 +21,31 @@ bpsk_modulator #(
     .clk        (clk),
     .arst       (arst),
     .en         (en),
-    .s          (s),
+    .in         (in),
     .signal_out (signal_out)
 );
+
+assign sel_cnt = dut.sel_cnt;
+assign sine_cnt = dut.sine_cnt;
+assign sel = dut.sel;
 
 initial 
     begin        
         clk = 0; 
         #1; arst = 1; en = 0;
         #1; arst = 0; en = 1;
-        for (i = 0; i <= 511; i = i + 1)
+        for (i = 0; i <= 5000; i = i + 1)
             begin
-                #1; s = $urandom_range(0,1); 
+                #1; in = $urandom_range(0,511); 
             end 
     end
 
 always #1 clk = !clk;
 
 initial 
-    $monitor("time=%g, clk=%b, s=%b, signal_out", $time, clk, s, signal_out);
+    $monitor("time=%g, clk=%b, in=%b, signal_out", $time, clk, in, signal_out);
 	
 initial 
-	#600 $stop;
+	#5000 $stop;
 
 endmodule
