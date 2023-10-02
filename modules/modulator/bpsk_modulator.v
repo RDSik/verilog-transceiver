5f7fbf28bf1b1dabde0e1e419a5d764b7245cb93
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2023-10-02 16:55:40 +0300

    Fix modulator and transceiver

--- modules/modulator/bpsk_modulator.v
+++ modules/modulator/bpsk_modulator.v
@@ -15,7 +15,7 @@ reg [DATA_WIDTH-1:0] sine_rom [2**ADDR_WIDTH-1:0];
 reg [DATA_WIDTH-1:0] neg_sine_rom [2**ADDR_WIDTH-1:0];
 reg [ADDR_WIDTH-1:0]         sine_cnt; // output sine signal counter
 reg [$clog2(ADDR_WIDTH)-1:0] sel_cnt;  // select counter
-reg                              sel;  // register for input select signal
+reg [ADDR_WIDTH:0]           sel;      // register for input select signal
 
 initial 
     begin
@@ -32,11 +32,11 @@ always @(posedge clk or posedge arst)
             end 
         else if (en)
             begin
-                signal_out <= sel ? sine_rom[sine_cnt] : neg_sine_rom[sine_cnt];     
+                signal_out <= sel[sel_cnt] ? sine_rom[sine_cnt] : neg_sine_rom[sine_cnt];     
                 sine_cnt <= sine_cnt + 1;
                 if (sine_cnt == 8'd255) // one period of sine
                     begin
-                        sel <= in[sel_cnt];
+                        sel <= in;
                         sel_cnt <= sel_cnt + 1;
                     end                
             end
