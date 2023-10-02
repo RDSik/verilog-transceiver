732c290e7b648512213fbf77ce271e432a23a4cd
Dmitry <131966248+RDSik@users.noreply.github.com>
2023-10-02 19:12:17 +0300

    Update decoder.v

--- modules/decoder/decoder.v
+++ modules/decoder/decoder.v
@@ -5,7 +5,7 @@ module decoder #(
 ) (
     input  wire                  clk,
     input  wire                  arst, // asynchronous reset
-    input  wire [DATA_WIDTH:0]   data,
+    input  wire [DATA_WIDTH:0]   in,
     output reg  [DATA_WIDTH-1:0] out_byte,
     output reg                   err
 );
@@ -19,13 +19,13 @@ module decoder #(
             end
             else if (!parity) begin
                 err <= 0;
-                out_byte <= data[7:0];
+                out_byte <= in[7:0];
             end
             else begin
                 err <= 1;
             end
         end
 
-    assign parity = (data[0]^data[1]^data[2]^data[3]^data[4]^data[5]^data[6]^data[7])^data[8];
+    assign parity = (in[0]^in[1]^in[2]^in[3]^in[4]^in[5]^in[6]^in[7])^in[8];
 
 endmodule
