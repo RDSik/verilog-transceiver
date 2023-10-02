a9a4d41503e2c71171e9a7bfe1c0f9c3460c3024
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2023-10-02 16:10:38 +0300

    Fix modulator and Manifest for hdlmakep

--- syn/Manifest.py
+++ syn/Manifest.py
@@ -4,7 +4,7 @@ action = "synthesis"
 
 syn_device = "xc7z020clg484-1"
 syn_package = ""
-syn_grade = "-3"
+syn_grade = ""
 syn_top = "transceiver_top"
 syn_project = "transceiver"
 syn_tool = "vivado"
