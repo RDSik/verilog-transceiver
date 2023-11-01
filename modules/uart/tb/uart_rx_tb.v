52227faee5b241fc2aa2a98869129ca12e7fff9d
Dmitry Ryabikov <131966248+RDSik@users.noreply.github.com>
2023-11-01 20:33:28 +0300

    Update uart_rx_tb.v

--- modules/uart/tb/uart_rx_tb.v
+++ modules/uart/tb/uart_rx_tb.v
@@ -3,13 +3,14 @@
 module uart_rx_tb();
 
 reg        clk;
-reg        arst;
 reg        data;
 
-wire [7:0] clk_cnt;
+wire [3:0] clk_cnt;
 wire [2:0] bit_cnt;
-wire       rx_done;
-wire       done;
+wire       rx_dv;
+wire       rx_data_r;
+wire       rx_data;
+wire       dv;
 wire [2:0] state;
 wire [7:0] q;
 wire [7:0] rx_byte;
@@ -22,24 +23,23 @@ uart_rx #(
     .DATA_WIDTH (8)
 ) dut (
     .clk  (clk),
-    .arst (arst),
-    .done (done),
+    .dv   (dv),
     .data (data),
     .q    (q)
 );
 
 assign state = dut.state;
 assign rx_byte = dut.rx_byte;
-assign rx_done = dut.rx_done;
+assign rx_dv = dut.rx_dv;
+assign rx_data_r = dut.rx_data_r;
+assign rx_data = dut.rx_data;
 assign clk_cnt = dut.clk_cnt;
 assign bit_cnt = dut.bit_cnt;
 
 initial 
     begin        
         clk = 0; 
-        #1; arst = 1;
-        #1; arst = 0;
-        for (i = 0; i <= 255; i = i + 1)
+        for (i = 0; i <= 1024; i = i + 1)
             begin
                 #1; data = $urandom_range(0,1); 
             end 
@@ -48,9 +48,9 @@ initial
 always #1 clk = ~clk;
 
 initial 
-    $monitor("time=%g, clk=%b, data=%b, done=%b, q=%b", $time, clk, data, done, q);
+    $monitor("time=%g, clk=%b, data=%b, dv=%b, q=%b", $time, clk, data, dv, q);
 	
 initial 
-	#300 $stop;
+	#1000 $stop;
 
 endmodule
