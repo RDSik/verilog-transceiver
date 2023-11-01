b9bee5d39e71086229e0f91a855c9a73018eaa31
Dmitry Ryabikov <131966248+RDSik@users.noreply.github.com>
2023-11-01 20:32:58 +0300

    Update uart_rx.v

--- modules/uart/uart_rx.v
+++ modules/uart/uart_rx.v
@@ -2,13 +2,13 @@
 
 module uart_rx #(
     parameter CLOCK_RATE = 1_000_000, // 1 MHz
-    parameter BAUD_RATE  = 115_200,
+    parameter BAUD_RATE  = 115_200, 
     parameter DATA_WIDTH = 8
 ) (
     input  wire                  clk,
     input  wire                  arst, // asynchronous reset
     input  wire                  data,
-    output wire                  done,
+    output wire                  dv, // data valid signal
     output wire [DATA_WIDTH-1:0] q
 );
     
@@ -19,9 +19,9 @@ module uart_rx #(
                      STOP    = 3'b011, 
                      CLEANUP = 3'b100;
 
-    reg                           rx_data_r = 1'b1;
-    reg                           rx_data   = 1'b1;
-    reg                           rx_done;
+    reg                           rx_data_r = 1;
+    reg                           rx_data   = 1;
+    reg                           rx_dv;
     reg [DATA_WIDTH-1:0]          rx_byte;
     reg [2:0]                     state;
     reg [$clog2(DATA_WIDTH)-1:0]  bit_cnt; // bit counter in rx_byte register
@@ -39,89 +39,93 @@ module uart_rx #(
         begin
             if (arst)
                 begin 
-                    bit_cnt <= 0;
-                    clk_cnt <= 0;
+                    state <= IDLE;               
                 end
             else
-                case (state)
-                    IDLE: 
-                        begin
-                            if (~data)
-                                begin
-                                    state <= START;
-                                end
-                            else
-                                begin
-                                    state <= IDLE;
-                                end
-                        end
-                    START:
-                        begin
-                            if (clk_cnt == (CLK_PER_BIT-1)/2)
-                                begin
-                                    if (data == 0)
-                                        begin
-                                            clk_cnt <= 0;
-                                            state <= RX_DATA;
-                                        end
-                                    else
-                                        begin
-                                            state <= IDLE;        
-                                        end
-                                end
-                            else
-                                begin
-                                    clk_cnt <= clk_cnt + 1;
-                                    state <= START;
-                                end
-                        end
-                    RX_DATA:
-                        begin
-                            if (clk_cnt < CLK_PER_BIT-1)
-                                begin
-                                    clk_cnt <= clk_cnt + 1;
-                                    state <= RX_DATA;
-                                end
-                            else
-                                begin 
-                                    clk_cnt <= 0;
-                                    rx_byte[bit_cnt] <= data;
-                                    if (bit_cnt < 7)
-                                        begin
-                                            bit_cnt <= bit_cnt + 1;
-                                            state <= RX_DATA;
-                                        end
-                                    else
-                                        begin 
-                                            bit_cnt <= 0;
-                                            state <= STOP;
-                                        end
-                                end
-                        end
-                    STOP:
-                        begin
-                            if (clk_cnt < CLK_PER_BIT-1)
-                                begin
-                                    clk_cnt <= clk_cnt + 1;
-                                    state <= STOP;
-                                end
-                            else
-                                begin 
-                                    rx_done <= 1;
-                                    clk_cnt <= 0;
-                                    state <= CLEANUP;
-                                end
-                        end
-                    CLEANUP:
-                        begin
-                            rx_done <= 0;
-                            state <= IDLE;
-                        end
-                    default: state <= IDLE;
-                endcase
+                begin
+                    case (state)
+                        IDLE: 
+                            begin
+                                bit_cnt <= 0;
+                                clk_cnt <= 0;
+                                rx_dv <= 0;
+                                if (data == 0)
+                                    begin
+                                        state <= START;
+                                    end
+                                else
+                                    begin
+                                        state <= IDLE;
+                                    end
+                            end
+                        START:
+                            begin
+                                if (clk_cnt == (CLK_PER_BIT-1)/2)
+                                    begin
+                                        if (data == 0)
+                                            begin
+                                                clk_cnt <= 0;
+                                                state <= RX_DATA;
+                                            end
+                                        else
+                                            begin
+                                                state <= IDLE;        
+                                            end
+                                    end
+                                else
+                                    begin
+                                        clk_cnt <= clk_cnt + 1;
+                                        state <= START;
+                                    end
+                            end
+                        RX_DATA:
+                            begin
+                                if (clk_cnt < CLK_PER_BIT-1)
+                                    begin
+                                        clk_cnt <= clk_cnt + 1;
+                                        state <= RX_DATA;
+                                    end
+                                else
+                                    begin 
+                                        clk_cnt <= 0;
+                                        rx_byte[bit_cnt] <= data;
+                                        if (bit_cnt < 7)
+                                            begin
+                                                bit_cnt <= bit_cnt + 1;
+                                                state <= RX_DATA;
+                                            end
+                                        else
+                                            begin 
+                                                bit_cnt <= 0;
+                                                state <= STOP;
+                                            end
+                                    end
+                            end
+                        STOP:
+                            begin
+                                if (clk_cnt < CLK_PER_BIT-1)
+                                    begin
+                                        clk_cnt <= clk_cnt + 1;
+                                        state <= STOP;
+                                    end
+                                else
+                                    begin 
+                                        rx_dv <= 1;
+                                        clk_cnt <= 0;
+                                        state <= CLEANUP;
+                                    end
+                            end
+                        CLEANUP:
+                            begin
+                                rx_dv <= 0;
+                                state <= IDLE;
+                            end
+                        default: state <= IDLE;
+                    endcase
+                end
         end
 
-    assign done = rx_done;
+    assign dv = rx_dv;
     assign q = rx_byte;
 
 endmodule
