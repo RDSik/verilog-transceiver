e97ce0ea3d4708a8c83ae1767184ac24358f0a29
Dmitry Ryabikov <131966248+RDSik@users.noreply.github.com>
2023-11-22 13:16:03 +0300

    Update hamming_top_tb.v

--- modules/hamming/tb/hamming_top_tb.v
+++ modules/hamming/tb/hamming_top_tb.v
@@ -9,7 +9,9 @@ reg [7:0]  data;
 wire [7:0]  q;
 wire [11:0] encoder_out;
 
-integer i;
+reg [7:0] temp1, temp2;
+
+integer pn, i;
 
 hamming_top dut (
     .clk        (clk),
@@ -21,22 +23,58 @@ hamming_top dut (
 assign encoder_out = dut.encoder_out;
 
 initial 
-    begin        
+    begin
+        pn = 0;
+        data = 0;
+       
+        forever begin
+            @ (posedge clk)
+            pn = {$random} %12;
+            #1
+            for (i=0; i<8; i=i+1) begin
+                if (i!= pn)
+                    data[i] = data[i];
+                else
+                    data[i] = data[i];
+            end
+        end
+    end
+   
+    always @ (posedge clk)
+        begin
+            temp1 <= data;
+            temp2 <= temp1;
+        end
+
+    always @ (*)
+    begin
+        #1
+            if (temp1 == q)
+                $display("OK:time=%0t data=%0d q=%0d", $time, temp2, q);
+            else
+                $error("ERROR:time=%0t data=%0d q=%0d", $time, temp2, q);
+    end
+       
+    initial begin
         clk = 1;
-        #1; rst = 0;
-        #1; rst = 1;
-        for (i = 0; i <= 20; i = i + 1)
-            begin
-                #4; data = $urandom_range(0,255); 
-            end 
+        rst = 0;
+        data = 0;
+        
+        #200
+        @ (posedge clk)
+        rst = 1;
+        
+        #200
+        forever begin
+            @ (posedge clk)
+            data = {$random} % 8'b1000_0000;
+            @ (posedge clk)
+            data = {$random} % 8'b1000_0000;
+        end
     end
 
-always #1 clk = ~clk;
+    always #10 clk = ~clk;
 
-initial 
-    $monitor("time=%g, clk=%b, data=%b, q=%b", $time, clk, data, q);
-	
-initial 
-	#100 $stop;
+    initial #5000 $stop;
 
 endmodule
