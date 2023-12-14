2b07d2ca32d65e14f6b05a3c5e50fafc072e508d
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2023-12-14 10:10:55 +0300

    Update

--- modules/bpsk/sine_value.m
+++ modules/bpsk/sine_value.m
@@ -1,19 +1,19 @@
 clc
 clear
 x = [0:2*pi/256:2*pi-2*pi/256];
-%%y = -sin(x) + 1;
+%% y = -sin(x) + 1;
 y = sin(x) + 1;
 y2 = y./max(y);
 y3 = ceil(y2.*(2^12-1));
 
-%%fid = fopen('neg_sine_value.dat', 'wb');
+%% fid = fopen('neg_sine_value.dat', 'wb');
 fid = fopen('sine_value.dat', 'wb');
 if fid == -1
     error('File is not opened');
 end
 
-for i=1:256
-    fprintf(fid, '%s\n', dec2bin(y3(i),12));
+for i = 1:256
+    fprintf(fid, '%s\n', dec2bin(y3(i), 12));
 end
 
 fclose(fid);
