67c0ba4075313046bc7470483442949ddbac833f
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2023-12-14 15:23:27 +0300

    auto assembly simulation is working now

--- syn/add_files.tcl
+++ syn/add_files.tcl
@@ -3,9 +3,9 @@ open_project transceiver.xpr
 read_ip ../platform/xilinx/clk_wiz/clk_wiz.xci
 generate_target all [get_files *clk_wiz.xci]
 
-read_mem ../modules/bpsk/sine_value.dat
+read_mem ../top/sim/sine_value.dat
 
-read_mem ../modules/bpsk/neg_sine_value.dat
+read_mem ../top/sim/neg_sine_value.dat
 
 close_project
 exit
