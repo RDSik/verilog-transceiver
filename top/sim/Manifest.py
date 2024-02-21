8e22a4da6cdc05853bab32739734a1e64f014d93
Dmitry Ryabikov <131966248+RDSik@users.noreply.github.com>
2024-02-21 16:16:06 +0300

    Update Manifest.py

--- top/sim/Manifest.py
+++ top/sim/Manifest.py
@@ -2,7 +2,7 @@ action = "simulation"
 sim_tool = "modelsim"
 sim_top = "transceiver_tb"
 
-sim_post_cmd ="vsim work.transceiver_tb"
+sim_post_cmd = "vsim work.transceiver_tb"
 
 modules = {
     "local" : [ "../tb/"]
