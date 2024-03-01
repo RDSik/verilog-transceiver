a9d39ff5e27d5a9b373adc418478b95519861125
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2024-03-01 17:03:41 +0300

    Fix add_files.tcl

--- syn/add_files.tcl
+++ syn/add_files.tcl
@@ -3,9 +3,9 @@ open_project transceiver.xpr
 read_ip ../platform/xilinx/clk_wiz/clk_wiz.xci
 generate_target all [get_files *clk_wiz.xci]
 
-read_mem ../top/sim/modelsim/sin_value.dat
+read_mem ../sim/modelsim/sin_value.dat
 
-read_mem ../top/sim/modelsim/neg_sin_value.dat
+read_mem ../sim/modelsim/neg_sin_value.dat
 
 close_project
 exit
