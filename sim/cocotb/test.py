75e254f595566d9946fa9fa4fd558699122667e2
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2024-03-11 13:32:01 +0300

    Add cocotb test

---
+++ sim/cocotb/test.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+
+import cocotb
+from cocotb.runner import get_runner
+
+def test_runner():
+    src = Path("../../")
+    
+    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
+    sim = os.getenv("SIM", "questa")
+    
+    build_dir = Path('sim_build_transceiver')
+    build_dir.mkdir(exist_ok=True)
+
+    verilog_sources = [
+        src / "top/transceiver_top.v",
+        src / "modules/bpsk/sin_generator.v",
+        src / "modules/bpsk/bpsk_modulator.v",
+        src / "modules/bpsk/bpsk_demodulator.v",
+        src / "modules/hamming/hamming_decoder.v",
+        src / "modules/hamming/hamming_encoder.v",
+        src / "modules/uart/UART/Verilog/source/UART_RX.v",
+        src / "modules/uart/UART/Verilog/source/UART_TX.v",
+    ]
+    hdl_toplevel = 'transceiver_top' # HDL module name
+    test_module = 'transceiver_tb' # Python module name
+
+    runner = get_runner(sim)
+    
+    runner.build(
+        verilog_sources=verilog_sources,
+        hdl_toplevel=hdl_toplevel,
+        build_dir=build_dir,
+        always=True, # Always rebuild project
+    )
+
+    runner.test(
+        hdl_toplevel=hdl_toplevel,
+        test_module=test_module,
+        waves=True,
+        gui=True,
+    )
---
+++ sim/cocotb/transceiver_tb.py
@@ -0,0 +1,24 @@
+import cocotb
+import random
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge, FallingEdge, ClockCycles
+
+@cocotb.test()
+async def test_transceiver(dut):
+
+    cocotb.start_soon(Clock(dut.clk, 2, units = 'ns').start())
+
+    # Reset and enable module
+    await FallingEdge(dut.clk)
+    dut.rst_n.value = 0
+    dut.en.value = 0
+    await Timer(2, units="ns")
+    dut.rst_n.value = 1
+    dut.en.value = 1
+    await RisingEdge(dut.clk)
+
+    # Input data module
+    for i in range(12500):
+        dut.data.value = random.randint(0, 1)
+        print(f"data={dut.data}, active={dut.active}, done={dut.done}, q={dut.q}")
+        await Timer(1, units="ns")
