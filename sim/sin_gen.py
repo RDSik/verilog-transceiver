0c56bb048c396ae73a16e5f4cb30be15d57c3ce5
Dmitry Ryabikov <dmitry.ryabikov@spbpu.com>
2024-03-25 15:02:02 +0300

    Add sine value generation with python but only 16,32,64 length

---
+++ sim/sin_gen.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# def float_to_binary(float_num):
+    # import struct
+    # return ''.join(format(c, '08b') for c in struct.pack('!f', float_num))
+
+def float_to_binary(float_num):
+    from bitstring import BitArray
+    bit_array = BitArray(float=float_num, length=16)
+    return bit_array.bin
+
+polarity = str(input())
+
+if polarity == 'pos':
+    file_name = 'sin_value.dat'
+    with open(file_name, 'w') as s:
+        t = np.arange(0, 2*np.pi-2*np.pi/256, 2*np.pi/256)
+        sin_array = np.sin(t)
+        for i in range(255):
+            s.write(float_to_binary(sin_array[i]) + '\n')
+elif polarity == 'neg':
+    file_name = polarity + '_sin_value.dat'
+    with open(file_name, 'w') as s:
+        t = np.arange(0, 2*np.pi-2*np.pi/256, 2*np.pi/256)
+        sin_array = -np.sin(t)
+        for i in range(255):
+            s.write(float_to_binary(sin_array[i]) + '\n')
