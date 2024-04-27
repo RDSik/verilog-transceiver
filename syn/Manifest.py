6ec07526def9aa299f7185894992f27b0cc98761
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-04-27 13:58:32 +0300

    Update Manifest.py

--- syn/Manifest.py
+++ syn/Manifest.py
@@ -5,8 +5,10 @@ action = "synthesis"
 syn_device = "xc7z020clg484"
 syn_grade = "-1"
 syn_package = ""
+
 syn_top = "transceiver_top"
 syn_project = "transceiver"
+
 syn_tool = "vivado"
 
 syn_post_project_cmd = "vivado -mode tcl -source add_files.tcl"
