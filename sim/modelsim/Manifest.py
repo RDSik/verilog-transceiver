90333f61415d4874f341dc958a040a96a6798ece
Dmitry <dmitryryabickov@yandex.ru>
2024-07-27 23:06:46 +0300

    Update macro file for modelsim simulation

--- sim/modelsim/Manifest.py
+++ sim/modelsim/Manifest.py
@@ -5,7 +5,7 @@ action = "simulation"
 sim_tool = "modelsim"
 sim_top = "transceiver_tb"
 
-sim_post_cmd = "vsim -do vsim.do -i transceiver_tb"
+sim_post_cmd = "vsim -do wave.do -i transceiver_tb"
 
 modules = {
     "local" : [ 
--- sim/modelsim/vsim.do
+++
@@ -1,5 +0,0 @@
-vcd file transceiver_tb.vcd;
-add log -r /*
-add wave sim:/dut/*
-run -all
-wave zoom full
