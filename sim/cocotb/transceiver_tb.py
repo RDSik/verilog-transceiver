ae5dfccf1a28b51e4a545601d41bd593adfd203d
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-08-14 20:08:52 +0300

    Update transceiver_tb.py

--- sim/cocotb/transceiver_tb.py
+++ sim/cocotb/transceiver_tb.py
@@ -6,19 +6,27 @@ from cocotb.triggers import Timer, RisingEdge, FallingEdge, ClockCycles
 @cocotb.test()
 async def test_transceiver(dut):
 
-    cocotb.start_soon(Clock(dut.clk, 2, units = 'sec').start())
+    clk_per = 2
+    
+    cocotb.start_soon(Clock(dut.clk, clk_per, units = 'sec').start())
 
-    # Reset and enable module
-    await FallingEdge(dut.clk)
-    dut.arstn.value = 0
-    dut.en.value = 0
-    await Timer(2, units="sec")
-    dut.arstn.value = 1
-    dut.en.value = 1
+    # Reset and enable
+    async def rst_en(dut, cycles):
+        await FallingEdge(dut.clk)
+        dut.arstn.value = 0
+        dut.en.value = 0
+        await ClockCycles(dut.clk, cycles)
+        dut.arstn.value = 1
+        dut.en.value = 1
+
+    # Input data
+    async def data_gen():
+        for i in range(25000):
+            dut.data.value = random.randint(0, 1)
+            print(f"uart_rx_out={dut.uart_rx_out}, encoder_out={dut.encoder_out}, decoder_out={dut.decoder_out}, demodulator_out={dut.demodulator_out}, active={dut.active}, data_valid={dut.data_valid}, done={dut.done}")
+            await Timer(clk_per/2, units="sec")
+
+    #------------------Order of test execution -------------------
+    await rst_en(dut, 1)
     await RisingEdge(dut.clk)
-
-    # Input data module
-    for i in range(25000):
-        dut.data.value = random.randint(0, 1)
-        print(f"uart_rx_out={dut.uart_rx_out}, encoder_out={dut.encoder_out}, decoder_out={dut.decoder_out}, demodulator_out={dut.demodulator_out}, active={dut.active}, data_valid={dut.data_valid}, done={dut.done}")
-        await Timer(1, units="sec")
+    await data_gen()
