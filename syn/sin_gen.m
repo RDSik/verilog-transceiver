f4b2cdad46f0b785971e22145db29c1b226f0e1f
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2024-09-10 11:54:47 +0300

    Update sin_gen.m

--- syn/sin_gen.m
+++ syn/sin_gen.m
@@ -1,19 +1,32 @@
 clc
 clear
 x = [0:2*pi/256:2*pi-2*pi/256];
-%%y = -sin(x) + 1;
-y = sin(x) + 1;
-y2 = y./max(y);
+y1 = sin(x) + 1;
+y2 = y1./max(y1);
 y3 = ceil(y2.*(2^12-1));
 
-%%fid = fopen('neg_sin_val.dat', 'wb');
-fid = fopen('sin_val.dat', 'wb');
-if fid == -1
+fid1 = fopen('sin_val.dat', 'wb');
+if fid1 == -1
     error('File is not opened');
 end
 
 for i = 1:256
-    fprintf(fid, '%s\n', dec2bin(y3(i), 12));
+    fprintf(fid1, '%s\n', dec2bin(y3(i), 12));
 end
 
-fclose(fid);
+fclose(fid1);
+
+y4 = -sin(x) + 1;
+y5 = y4./max(y4);
+y6 = ceil(y5.*(2^12-1));
+
+fid2 = fopen('neg_sin_val.dat', 'wb');
+if fid2 == -1
+    error('File is not opened');
+end
+
+for i = 1:256
+    fprintf(fid1, '%s\n', dec2bin(y6(i), 12));
+end
+
+fclose(fid2);
